85c442683c7538203f90b59fdeedf4181c3d9e10
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-02-20 15:51:00 -0500

    Se agrega variable de userReporsitory con la función para obtener las publicaciones

--- app/src/main/java/com/android/testceiba/userdetail/viewmodel/UserDetailViewModel.kt
+++ app/src/main/java/com/android/testceiba/userdetail/viewmodel/UserDetailViewModel.kt
@@ -1,7 +1,13 @@
 package com.android.testceiba.userdetail.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.android.testceiba.api.UserRepository
 
 class UserDetailViewModel: ViewModel() {
-    
+
+    var mUserRepository: UserRepository? = null
+
+    fun getPosts() {
+        
+    }
 }
