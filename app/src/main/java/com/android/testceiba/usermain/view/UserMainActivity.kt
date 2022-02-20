f1495066641e135f96935977798099d5a5900575
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-02-20 13:31:47 -0500

    Se agrega función observables donde se da manejo a los observadores de la lista mutable y se agrega dicha función para la lista mutable de getUserLiveData, también se inicializa el adaptador y se crea variable con la lista de usuarios

--- app/src/main/java/com/android/testceiba/usermain/view/UserMainActivity.kt
+++ app/src/main/java/com/android/testceiba/usermain/view/UserMainActivity.kt
@@ -9,11 +9,17 @@ import com.android.testceiba.api.UserRepository
 import com.android.testceiba.api.WebService
 import com.android.testceiba.databinding.ActivityMainBinding
 import com.android.testceiba.db.UserDBImplement
+import com.android.testceiba.usermain.model.User
+import com.android.testceiba.usermain.view.adapter.UserMainAdapter
 import com.android.testceiba.usermain.viewmodel.UserMainViewModel
 
 class UserMainActivity : AppCompatActivity() {
     private var mMainBinding: ActivityMainBinding? = null
     private var mMainViewModel: UserMainViewModel? = null
+
+    private var mUserMainAdapter: UserMainAdapter? = null
+
+    private var dataListUser: List<User> = ArrayList()
 
     companion object {
         const val DB_NAME = "USER_DB"
@@ -24,9 +30,16 @@ class UserMainActivity : AppCompatActivity() {
         mMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mMainBinding?.root)
 
+        mUserMainAdapter = UserMainAdapter(this, dataListUser)
         mMainViewModel = ViewModelProvider(this)[UserMainViewModel::class.java]
         mMainViewModel?.mUserRepository = UserRepository(WebService.getInstance())
         mMainViewModel?.mRoomImpl = Room.databaseBuilder(this, UserDBImplement::class.java, DB_NAME).build()
         mMainViewModel?.getUsers()
+    }
+
+    fun observables() {
+        mMainViewModel?.getUserLiveData.observe(this, {
+
+        })
     }
 }
