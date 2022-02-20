601d120623560c1116d444578703545944be6271
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-02-20 16:52:44 -0500

    Se agrega variable de la lista de las publicaciones y se inicializa el adapatador de detalle junto con el recyclerview de las publicaciones

--- app/src/main/java/com/android/testceiba/userdetail/view/UserDetailActivity.kt
+++ app/src/main/java/com/android/testceiba/userdetail/view/UserDetailActivity.kt
@@ -2,25 +2,33 @@ package com.android.testceiba.userdetail.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.testceiba.R
-import com.android.testceiba.databinding.ActivityMainBinding
+import com.android.testceiba.databinding.ActivityUserDetailBinding
+import com.android.testceiba.userdetail.model.Post
 import com.android.testceiba.userdetail.view.adapter.UserDetailAdapter
 import com.android.testceiba.userdetail.viewmodel.UserDetailViewModel
 
 class UserDetailActivity : AppCompatActivity() {
-    private var mActivityUserDetailBinding: ActivityMainBinding? = null
+    private var mActivityUserDetailBinding: ActivityUserDetailBinding? = null
 
     private var mDetailAdapter: UserDetailAdapter? = null
 
     private var mDetailVieModel: UserDetailViewModel? = null
 
+    private var dataListPost = ArrayList<Post>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mActivityUserDetailBinding = ActivityMainBinding.inflate(layoutInflater)
+        mActivityUserDetailBinding = ActivityUserDetailBinding.inflate(layoutInflater)
         setContentView(mActivityUserDetailBinding?.root)
     }
 
     fun initializeWidgets() {
-        
+        mDetailAdapter = UserDetailAdapter(this, dataListPost)
+        with(mActivityUserDetailBinding?.listPost) {
+            this?.layoutManager = LinearLayoutManager(this@UserDetailActivity)
+            this?.adapter = mDetailAdapter
+        }
     }
 }
