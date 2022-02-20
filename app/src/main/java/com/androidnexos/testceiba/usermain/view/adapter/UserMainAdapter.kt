c3625c1a34c41dd42f32979722c5994a55d65cee
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-02-19 20:52:55 -0500

    Se hereda la clase del adaptador de Recycler.ViewHolder y se implementan los métodos para el uso del adaptador

--- app/src/main/java/com/androidnexos/testceiba/usermain/view/adapter/UserMainAdapter.kt
+++ app/src/main/java/com/androidnexos/testceiba/usermain/view/adapter/UserMainAdapter.kt
@@ -1,10 +1,26 @@
 package com.androidnexos.testceiba.usermain.view.adapter
 
 import android.content.Context
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.androidnexos.testceiba.usermain.model.User
 
 class UserMainAdapter(private val context: Context, private val listUsers: List<User>):
-        RecyclerView.Adapter<>{
-            
+        RecyclerView.Adapter<UserMainAdapter.ViewHolderUser>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderUser {
+                TODO("Not yet implemented")
+        }
+
+        override fun onBindViewHolder(holder: ViewHolderUser, position: Int) {
+                TODO("Not yet implemented")
+        }
+
+        override fun getItemCount(): Int {
+                TODO("Not yet implemented")
+        }
+
+        class ViewHolderUser(): RecyclerView.ViewHolder() {
+
+        }
+
 }
