2a1db4d049c04b2486a9ef7ac66a0abbc60f4466
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-02-19 21:45:48 -0500

    Se modifica nombre del paquete de UserMainActivity en el manifest y se agrega corrutina que ejecuta la obtención de usuarios a través del endpoint de getUsers

--- app/src/main/java/com/android/testceiba/usermain/viewmodel/UserMainViewModel.kt
+++ app/src/main/java/com/android/testceiba/usermain/viewmodel/UserMainViewModel.kt
@@ -1,16 +1,28 @@
 package com.android.testceiba.usermain.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.android.testceiba.api.UserRepository
 import com.android.testceiba.usermain.model.User
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class UserMainViewModel(private val userRepository: UserRepository): ViewModel() {
+class UserMainViewModel: ViewModel() {
     val getUserLiveData = MutableLiveData<List<User>>()
+    var userRepository: UserRepository? = null
 
     fun getUsers() {
-
+        CoroutineScope(Dispatchers.IO).launch {
+            val response = userRepository?.getUsers()
+            withContext(Dispatchers.Main) {
+                if (response?.isSuccessful!!) {
+                    Log.e("USERS", response.body().toString())
+                }
+            }
+        }
     }
 
 }
