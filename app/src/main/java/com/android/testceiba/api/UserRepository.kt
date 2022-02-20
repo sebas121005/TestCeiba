a861993b3bead12d4a165685a91d2f2affda7129
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-02-20 15:44:49 -0500

    Se agrega función que instancia el endpoint de que obtiene las publicaciones y se agrega variable de id para especificar el id del usuario

--- app/src/main/java/com/android/testceiba/api/UserRepository.kt
+++ app/src/main/java/com/android/testceiba/api/UserRepository.kt
@@ -1,5 +1,7 @@
 package com.android.testceiba.api
 
 class UserRepository(private val webService: WebService) {
+    var userId: String? = null
     suspend fun getUsers() = webService.getUsers()
+    suspend fun getPost() = webService.getPosts(userId!!)
 }
