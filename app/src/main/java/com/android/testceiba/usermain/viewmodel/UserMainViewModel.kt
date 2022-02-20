84aed0828ebf32599f9d4d45d7f89a1818660b94
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-02-19 21:34:24 -0500

    Se agrega lista mutable para mostrar los usuarios y la función donde se va ejecutar el consumo del endpoint de user

--- app/src/main/java/com/android/testceiba/usermain/viewmodel/UserMainViewModel.kt
+++ app/src/main/java/com/android/testceiba/usermain/viewmodel/UserMainViewModel.kt
@@ -1,8 +1,16 @@
 package com.android.testceiba.usermain.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.android.testceiba.api.UserRepository
+import com.android.testceiba.usermain.model.User
+import kotlinx.coroutines.Job
 
 class UserMainViewModel(private val userRepository: UserRepository): ViewModel() {
+    val getUserLiveData = MutableLiveData<List<User>>()
+
+    fun getUsers() {
+
+    }
 
 }
