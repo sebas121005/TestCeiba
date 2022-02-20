b1871c6acbf2b5048d6761d925590c8bd8f6818d
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-02-20 14:53:32 -0500

    Se agrega activity para mostrar el detalle del usuario al momento de ver las publicaciones

---
+++ app/src/main/java/com/android/testceiba/userdetail/view/UserDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.android.testceiba.userdetail.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.testceiba.R
+
+class UserDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_detail)
+    }
+}
