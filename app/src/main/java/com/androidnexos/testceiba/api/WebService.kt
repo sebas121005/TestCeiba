6fd62738b279d37e7d7d7d7a55f3e0cfe5be2650
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-02-19 20:35:19 -0500

    Se agrega objeto que tiene la configuración del retofit junto con la url a la que se va apuntar para realizar el consumo de los endpoint

--- app/src/main/java/com/androidnexos/testceiba/api/WebService.kt
+++ app/src/main/java/com/androidnexos/testceiba/api/WebService.kt
@@ -2,9 +2,26 @@ package com.androidnexos.testceiba.api
 
 import com.androidnexos.testceiba.usermain.model.User
 import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 interface WebService {
     @GET("users/")
     suspend fun getAllMovies() : Response<List<User>>
+
+    companion object {
+        var webService: WebService? = null
+        fun getInstance() : WebService {
+            if (webService == null) {
+                val retrofit = Retrofit.Builder()
+                    .baseUrl("https://jsonplaceholder.typicode.com/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+                webService = retrofit.create(WebService::class.java)
+            }
+            return webService!!
+        }
+
+    }
 }
