d47229147ebc8043e56ccdaa7016f5ee4b5a9579
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-02-20 16:48:00 -0500

    Se agregan variables para el adaptador y el viewModel del detalle

--- app/src/main/java/com/android/testceiba/userdetail/view/UserDetailActivity.kt
+++ app/src/main/java/com/android/testceiba/userdetail/view/UserDetailActivity.kt
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.testceiba.R
 import com.android.testceiba.databinding.ActivityMainBinding
+import com.android.testceiba.userdetail.view.adapter.UserDetailAdapter
+import com.android.testceiba.userdetail.viewmodel.UserDetailViewModel
 
 class UserDetailActivity : AppCompatActivity() {
     private var mActivityUserDetailBinding: ActivityMainBinding? = null
+
+    private var mDetailAdapter: UserDetailAdapter? = null
+
+    private var mDetailVieModel: UserDetailViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +21,6 @@ class UserDetailActivity : AppCompatActivity() {
     }
 
     fun initializeWidgets() {
-
+        
     }
 }
