6525f5510ff3d84c7f1beaa2bd73f10385f9ff9e
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-02-20 16:39:47 -0500

    Se crea ViewHolder para uso del adaptador del detalle y se implementan métodos del adaptador

--- app/src/main/java/com/android/testceiba/userdetail/view/adapter/UserDetailAdapter.kt
+++ app/src/main/java/com/android/testceiba/userdetail/view/adapter/UserDetailAdapter.kt
@@ -1,8 +1,30 @@
 package com.android.testceiba.userdetail.view.adapter
 
 import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.android.testceiba.databinding.ItemPostBinding
 import com.android.testceiba.userdetail.model.Post
 
-class UserDetailAdapter (private val context: Context, private val listPost: List<Post>) {
-    
+class UserDetailAdapter(private val context: Context, private val listPost: List<Post>)
+    : RecyclerView.Adapter<UserDetailAdapter.ViewHolderDetail>(){
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderDetail {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolderDetail, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class ViewHolderDetail(private val itemPostBinding: ItemPostBinding):
+                    RecyclerView.ViewHolder(itemPostBinding.root) {
+
+
+    }
 }
