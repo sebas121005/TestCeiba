c16e89075e614d4a6f69af339d1b8a6079266c15
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-02-20 15:54:37 -0500

    Se implementa corrutina para la obtención de las publicaciones y se agrega validación en caso de que la respuesta sea exitosa

--- app/src/main/java/com/android/testceiba/userdetail/viewmodel/UserDetailViewModel.kt
+++ app/src/main/java/com/android/testceiba/userdetail/viewmodel/UserDetailViewModel.kt
@@ -2,12 +2,20 @@ package com.android.testceiba.userdetail.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.android.testceiba.api.UserRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class UserDetailViewModel: ViewModel() {
 
     var mUserRepository: UserRepository? = null
 
     fun getPosts() {
-        
+        CoroutineScope(Dispatchers.IO).launch {
+            val response = mUserRepository?.getPost()
+            if (response?.isSuccessful!!) {
+                
+            }
+        }
     }
 }
