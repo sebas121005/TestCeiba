881a75a9476243a8e26a590eba80e271126bec55
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-02-19 20:41:30 -0500

    Se agrega clase UserMainViewModel con parámetro de userRepository en el constructor para acceder a la función que permite obtener los usuarios

---
+++ app/src/main/java/com/androidnexos/testceiba/usermain/viewmodel/UserMainViewModel.kt
@@ -0,0 +1,8 @@
+package com.androidnexos.testceiba.usermain.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.androidnexos.testceiba.api.UserRepository
+
+class UserMainViewModel(private val userRepository: UserRepository): ViewModel() {
+
+}
