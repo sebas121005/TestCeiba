10fa5294221dcf3c094f84b5ed103074bce87684
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-02-19 20:46:54 -0500

    Se agrega clase UserMainAdapater que contendrá el adaptador para mostrar la lista de lo usuarios

---
+++ app/src/main/java/com/androidnexos/testceiba/usermain/view/adapter/UserMainAdapter.kt
@@ -0,0 +1,10 @@
+package com.androidnexos.testceiba.usermain.view.adapter
+
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+import com.androidnexos.testceiba.usermain.model.User
+
+class UserMainAdapter(private val context: Context, private val listUsers: List<User>):
+        RecyclerView.Adapter<>{
+            
+}
