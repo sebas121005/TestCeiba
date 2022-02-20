7189690d34b7d94c21770f5258e58d9fd9509ad9
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-02-19 22:00:21 -0500

    Se crea UserDB para hacer uso de la base de datos con Room y separar responsabilidades de la clase que obtiene los usuarios del endpoint a la que da manejo a la BD

--- app/src/main/java/com/android/testceiba/usermain/model/User.kt
+++ app/src/main/java/com/android/testceiba/usermain/model/User.kt
@@ -1,7 +1,17 @@
 package com.android.testceiba.usermain.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
-data class User(@SerializedName("name") var name: String,
+
+data class User(@SerializedName("id") var id: Int,
+                @SerializedName("name") var name: String,
                 @SerializedName("phone") var phone: String,
                 @SerializedName("email") var email: String)
+
+@Entity
+data class UserDB(@PrimaryKey var id: Int,
+                  var name: String,
+                  var phone: String,
+                  var email: String)
