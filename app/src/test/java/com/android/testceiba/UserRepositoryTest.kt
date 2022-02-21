730d379a7a74ffae032de39620d71ecde36abb12
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-02-21 14:25:03 -0500

    Se agrega clase de pruebas unitarias de UserRepository y se agrega prueba que valida el tamaño de la lista que se obtiene de los usuarios

---
+++ app/src/test/java/com/android/testceiba/UserRepositoryTest.kt
@@ -0,0 +1,24 @@
+package com.android.testceiba
+
+import com.android.testceiba.api.UserRepository
+import com.android.testceiba.api.WebService
+import com.android.testceiba.usermain.model.User
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Test
+import retrofit2.Response
+
+class UserRepositoryTest {
+    val userRepository = UserRepository(WebService.getInstance())
+    @Test
+    fun getUsers_isCorrect_fullList() = runBlocking {
+        //Arrange
+        val sizeList = 0
+
+        //Act
+        val result: Response<List<User>> = userRepository.getUsers()
+
+        //Assert
+        Assert.assertNotEquals(sizeList, result.body()?.size)
+    }
+}
